7465f7ebeeebae9a8de7201a25e4ba2944a0edc0
Piotr Puszkarski <piotrpuszk@gmail.com>
2023-08-31 16:58:54 +0200

    End of section 6

--- src/AuctionService/Consumers/AuctionCreatedFaultConsumer.cs
+++ src/AuctionService/Consumers/AuctionCreatedFaultConsumer.cs
@@ -5,21 +5,21 @@ namespace AuctionService;
 
 public class AuctionCreatedFaultConsumer : IConsumer<Fault<AuctionCreated>>
 {
-    public async Task Consume(ConsumeContext<Fault<AuctionCreated>> context)
+  public async Task Consume(ConsumeContext<Fault<AuctionCreated>> context)
+  {
+    System.Console.WriteLine("--> Consuming faulty creation");
+
+    var exception = context.Message.Exceptions.First();
+
+    if (exception.ExceptionType == "System.ArgumentException")
     {
-      System.Console.WriteLine("--> Consuming faulty creation");
+      context.Message.Message.Model = "FooBar";
 
-      var exception = context.Message.Exceptions.First();
-
-      if(exception.ExceptionType == "System.ArgumentException")
-      {
-        context.Message.Message.Model = "FooBar";
-
-        await context.Publish(context.Message.Message);
-      }
-      else
-      {
-        System.Console.WriteLine("Not an argument exception - update error dashboard somewhere");
-      }
+      await context.Publish(context.Message.Message);
     }
+    else
+    {
+      System.Console.WriteLine("Not an argument exception - update error dashboard somewhere");
+    }
+  }
 }
